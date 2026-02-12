00a9cc39ee426385b60cd0bb1c80b44670f6dce1
Adnan Ahmed <adnanahmedabdu@gmail.com>
2026-02-12 13:43:54 +0300

    feat(ui): basic login and singup form

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -3,7 +3,23 @@
 </script>
 
 <template>
-  <div>
-    Signup
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <form @submit.prevent class="fieldset bg-base-100 border-base-300 rounded-box w-xs border gap-5 p-4">
+        <fieldset class="fieldset">
+          <label class="label">Email</label>
+          <input type="email" class="input validator outline-0" placeholder="Email" required />
+          <p class="validator-hint hidden">Required</p>
+        </fieldset>
+
+        <label class="fieldset">
+          <span class="label">Password</span>
+          <input type="password" class="input validator outline-0" placeholder="Password" required />
+          <span class="validator-hint hidden">Required</span>
+        </label>
+
+        <button class="btn mt-4" type="submit">Sign Up</button>
+      </form>
+    </div>
   </div>
 </template>
